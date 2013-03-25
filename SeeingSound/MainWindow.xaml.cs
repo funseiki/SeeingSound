63c81c5d3a1ff2ef1a896d74fc2bdbda9c7eb4d4
Vinit Shah <elysiums.shade@gmail.com>
2013-03-24 22:04:42 -0500

    Connecting to the Kinect

--- SeeingSound/MainWindow.xaml.cs
+++ SeeingSound/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,49 @@ namespace SeeingSound
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        protected KinectSensor sensor;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void setStatus(String text)
+        {
+            KinectText.Text = text;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            setStatus("Kinecting...");
+            foreach(var potential_sensor in KinectSensor.KinectSensors)
+            {
+                if(potential_sensor.Status == KinectStatus.Connected)
+                {
+                    sensor = potential_sensor;
+                    break;
+                }
+            }
+
+            if(sensor != null)
+            {
+                try
+                {
+                    sensor.Start();
+                    setStatus("Kinected!");
+                }
+                catch(IOException)
+                {
+                    sensor = null;
+                    setStatus("Sensor is in use elsewhere");
+                }
+            }
+            else
+            {
+                setStatus("Unable to find the Kinect");
+            }
+        }
+
     }
 }
