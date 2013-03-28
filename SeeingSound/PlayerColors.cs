a5634c969cf46bb409c733c8646e94f606ae2aac
Vinit Shah <elysiums.shade@gmail.com>
2013-03-27 19:04:05 -0500

    Set opacity to about 30%

--- SeeingSound/PlayerColors.cs
+++ SeeingSound/PlayerColors.cs
@@ -10,13 +10,13 @@ namespace SeeingSound
     class PlayerColors
     {
         public static System.Windows.Media.Color[] Colors = {
-                                          Color.FromArgb(255, 30, 56, 250),
-                                          Color.FromArgb(255, 250, 85, 30),
-                                          Color.FromArgb(255, 250, 30, 34),
-                                          Color.FromArgb(255, 52, 250, 30),
-                                          Color.FromArgb(255, 211, 54, 255),
-                                          Color.FromArgb(255, 255, 54, 205),
-                                          Color.FromArgb(255, 54, 255, 242)
+                                          Color.FromArgb(76, 30, 56, 250),
+                                          Color.FromArgb(76, 250, 85, 30),
+                                          Color.FromArgb(76, 250, 30, 34),
+                                          Color.FromArgb(76, 52, 250, 30),
+                                          Color.FromArgb(76, 211, 54, 255),
+                                          Color.FromArgb(76, 255, 54, 205),
+                                          Color.FromArgb(76, 54, 255, 242)
                                       };
 
 
